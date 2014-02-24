bef3533713ef8f037bea6394e24944a2aabd61c4
winterfell <winterfall.ding@gmail.com>
2014-02-24 14:21:41 +0800

    insert sort

---
+++ chap2/insert_sort.py
@@ -0,0 +1,17 @@
+import copy
+
+def insert_sort(numbers):
+  if len(numbers) <= 1:
+    return numbers
+  for i in range(1, len(numbers)):
+    insert_one = numbers[i]
+    j = i - 1
+    while j >= 0 and insert_one < numbers[j]:
+      numbers[j + 1] = numbers[j]
+      j = j - 1
+    numbers[j + 1] = insert_one
+  return numbers
+
+if __name__ == '__main__':
+  numArray = [31, 41, 59, 26, 41, 58]
+  print insert_sort(numArray)
